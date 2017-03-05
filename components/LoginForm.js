c67d494660db955a84b1143e06262d9716fc850b
Yechiel K <chilikkal@gmail.com>
2017-03-05 01:25:17 -0500

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,50 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.updateUser = this.updateUser.bind(this)
+    this.updatePassword = this.updatePassword.bind(this)
+    this.submitEvent = this.submitEvent.bind(this)
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  updateUser(event) {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  updatePassword(event) {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+  submitEvent(event) {
+    event.preventDefault();
+    if (!this.state.username || !this.state.password) {
+      return null
+    }
+    
+    this.props.onSubmit()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitEvent}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.updateUser}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.updatePassword}/>
           </label>
         </div>
         <div>
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.handleChange = this.handleChange.bind(this)
+
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <p>Charachters left: {this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
