de673f06ad0f2674078beb6bec8b9a87386c75de
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-03-30 19:54:04 +0000

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,10 +1,28 @@
 const React = require('react');
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.submitIfFull = this.submitIfFull.bind(this);
+
+    this.changeState = this.changeState.bind(this);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  submitIfFull(event) {
+    event.preventDefault();
+  }
+
+  changeState(event) {
+    var key = event.target.id.split("-")[1]
+    this.setState({
+      key: event.target.value
+    });
   }
 
   render() {
@@ -13,21 +31,25 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input onChange={this.changeState} id="test-username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input onChange={this.changeState} id="test-password" type="password" />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button onClick={this.submitIfFull} type="submit">Log in</button>
         </div>
       </form>
     );
   }
+}
+
+LoginForm.propTypes = {
+  onSubmit: React.PropTypes.func
 }
 
 module.exports = LoginForm;
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -1,20 +1,36 @@
 const React = require('react');
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: props.maxChars
+    };
+
+    this.updateCharsLeft = this.updateCharsLeft.bind(this);
+  }
+
+  updateCharsLeft(event) {
+    this.setState({
+      value: this.props.maxChars - event.target.value.length
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input onChange={this.updateCharsLeft} type="text" />
+        <small>{this.state.value} characters left</small>
       </div>
     );
   }
+}
+
+TwitterMessage.propTypes = {
+  maxChars: React.PropTypes.number,
+  value: React.PropTypes.string
 }
 
 module.exports = TwitterMessage;
