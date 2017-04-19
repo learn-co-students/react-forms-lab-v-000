356052a58b445808f7611c761f70c8c6128e46d7
Pedro Acosta <pedro.acosta613@gmail.com>
2017-04-19 15:18:13 +0000

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.username = this.username.bind(this);
+    this.password = this.password.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  username(e){
+    this.setState({
+      username: e.target.value
+    })
+  }
+  password(e){
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  submit(e){
+    e.preventDefault();
+    // const {username, password} = this.state;
+    // or
+    if (!this.state.username || !this.state.password){
+      return null
+    }
+    this.props.onSubmit();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input value={this.state.username} id="test-username" type="text" onChange={this.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input value={this.state.password} id="test-password" type="password" onChange={this.password}/>
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+function splitWords(poem){
+  var lines = poem.split("\n").filter(l => l)
+  if (lines.length === 3) {
+    var line1 = lines[0].split(" ").filter(l => l).length;
+    var line2 = lines[1].split(" ").filter(l => l).length;
+    var line3 = lines[2].split(" ").filter(l => l).length;
+    if (line1 === 5 && line2 === 3 && line3 === 5){
+      return true
+    }
+  }
+  return false
+}
 
 export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: '',
+      valid: false
+    };
+
+    this.poem = this.poem.bind(this);
+  }
+
+  poem(e){
+    this.setState({
+      poem: e.target.value,
+      valid: splitWords(e.target.value)
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.poem} onChange={this.poem}/>
+        {this.state.valid ? null : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
