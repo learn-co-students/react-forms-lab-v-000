7f523a5ba50b9db9da11430fee7be78815181f79
smithwebtek <brad@smithwebtek.com>
2017-05-19 19:21:09 -0400

    Done.

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -3,16 +3,43 @@ import React from 'react';
 export default class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {
+			tweet: '',
+		};
 
-    this.state = {};
+    this.handleChangeTweet = this.handleChangeTweet.bind(this);
+    this.handleSubmitTweet = this.handleSubmitTweet.bind(this);
   }
-
-  render() {
+ 
+  handleChangeTweet(event) {
+		this.setState({
+			tweet: event.target.value,
+		});
+	}
+ 
+  handleSubmitTweet(event) {
+		alert("Ok, you have Tweeted, now please open a window!")
+	}
+	
+	render() {
     return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" />
-      </div>
+			<div>
+				<form onSubmit={this.handleSubmitTweet}>
+					<label>
+						<strong>Your message:</strong>
+						<input type="text" value={this.state.tweet} onChange={this.handleChangeTweet} />
+						<span>{this.props.maxChars - this.state.tweet.length}</span>
+					</label>
+ 				</form>
+			</div>	
     );
   }
+}
+
+TwitterMessage.propTypes = {
+	maxChars: React.PropTypes.number,
+};
+
+TwitterMessage.defaultProps = {
+	maxChars: 140,
 }
