a38c7a5732a1882f6d7157e765e6c1078acc3428
github username <krishel.lasam@gmail.com>
2017-06-01 02:29:48 -0400

    Done.

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -3,15 +3,35 @@ import React from 'react';
 export default class PoemWriter extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      valid: false,
+      value: '',
+    };
+  }
 
-    this.state = {};
+  handleChange(event) {
+    var value = event.target.value
+    var valid = false
+    var lines = value.split("\n")
+    var checkLines = lines.length === 3
+    var checkWords = lines[0].trim().split(' ').length === 5 && lines[1].trim().split(' ').length === 3 && lines[2].trim().split(' ').length === 5
+
+    if (checkLines && checkWords) {
+      valid = true
+    }
+
+    this.setState({
+      value: value,
+      valid: valid
+    });
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange}/>
+        {this.state.valid ? null : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -3,15 +3,24 @@ import React from 'react';
 export default class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: ''
+    };
+  }
 
-    this.state = {};
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        {this.props.maxChars - this.state.value.length}
       </div>
     );
   }
