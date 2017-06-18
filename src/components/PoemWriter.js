cc8c5eca2a96058604a345ba5bbba2bf92377b30
Omar Mason <mr.omarmason@gmail.com>
2017-06-18 02:26:05 +0000

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,39 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      valid: false
+    };
+  }
+
+  handleChange = (event) => {
+    var poem =  event.target.value
+    var goodPoem = false
+    var lines = poem.split("\n").map(function (line) {
+      return line.trim().split(' ').length
+    })
+    if (lines === [5, 3, 5]) {
+      goodPoem = true
+      console.log(goodPoem);
+    }
+    console.log(lines);
+    this.setState({
+      value: poem,
+      valid: goodPoem
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.value}
+          onChange={this.handleChange}
         />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+        {this.state.valid ? null : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleClick = event =>{
+    this.setState({
+      value: event.target.value,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+          onChange={this.handleClick}
+          value={this.state.value}/>
+        <p>
+          Remaining characters {this.props.maxChars - this.state.value.length}
+        </p>
       </div>
     );
   }
