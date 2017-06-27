bad92840adf1f83c4e4d68b8e176dbaf50fd7047
Callie Brazil <cbrazil@gmail.com>
2017-06-27 11:03:46 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    if(this.state.username && this.state.password) {
+      this.props.onSubmit()
+    }
+
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleOnSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="text" name="password" value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
@@ -28,6 +49,10 @@ class LoginForm extends React.Component {
       </form>
     );
   }
+}
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func
 }
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: "",
+    };
+
+    this.writeTweet = this.writeTweet.bind(this);
+  }
+
+  writeTweet(event) {
+    this.setState({
+      tweet: event.target.value,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.writeTweet}/>
+        <em>{this.props.maxChars - this.state.tweet.length}</em>
       </div>
     );
   }
+}
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number,
+}
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
 }
 
 export default TwitterMessage;
