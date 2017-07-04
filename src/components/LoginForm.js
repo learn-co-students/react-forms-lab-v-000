d48509efb0d807f6c0fdc397647a01c113a48bb8
John Verdone <jmverdone@gmail.com>
2017-07-04 14:33:22 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types';
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    if(this.state.username && this.state.password) {
+      this.props.onSubmit()
+    }
+
   }
 
   render() {
     return (
-      <form>
+       <form onSubmit={this.handleOnSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password"  value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
@@ -29,5 +49,9 @@ class LoginForm extends React.Component {
     );
   }
 }
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func
+}
+
 
 export default LoginForm;
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,9 +4,9 @@ function validPoem(poem){
   const lines = poem.trim().split('\n')
 
   if (lines.length === 3  ) {
-    let lineOne = (lines[0].trim().split(' ').length == 5)
-    let lineTwo = (lines[1].trim().split(' ').length == 3)
-    let lineThree = (lines[2].trim().split(' ').length == 5)
+    let lineOne = (lines[0].trim().split(' ').filter(l => l).length === 5)
+    let lineTwo = (lines[1].trim().split(' ').filter(l => l).length === 3)
+    let lineThree = (lines[2].trim().split(' ').filter(l => l).length === 5)
     return (lineOne && lineTwo && lineThree)
   } else {
   return false
@@ -41,16 +41,9 @@ class PoemWriter extends React.Component {
         <textarea
           rows="3"
           cols="60"
-          alue={this.state.value} onChange={this.handleChange}
+          value={this.state.value} onChange={this.handleChange}
         />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          <p>{this.state.value}</p>
-          {(this.state.isValid) ?" " : "poem is not written in the right structure!"}
-
-        </div>
+      {!this.state.isValid ? <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div> : null}
       </div>
     );
   }
