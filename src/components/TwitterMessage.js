2f9f6e7e188f383815300eea04d034ce02e7de05
Idayat <github email address>
2017-07-03 20:48:44 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import PropTypes from 'react'
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+
+    this.newMessage = this.newMessage.bind(this)
   }
 
+
+  newMessage (ev){
+    console.log('test', ev.persist);
+    this.setState({
+      message: ev.target.value
+    })
+    // console.log("test2", ev.target);
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message} onChange={this.newMessage} />
+        <span>{this.props.maxChars -this.state.message.length}</span>
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+TwitterMessage.defaultProps ={
+  maxChars: 140
+}
+
+TwitterMessage.propTypes ={
+  maxChars: PropTypes.number
+}
