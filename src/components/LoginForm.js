4cf6b1975fe32f6d96e5b251dfe7087d4c73ec1c
David Kennell <davidkennell0@gmail.com>
2017-07-09 14:58:09 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,51 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.submitForm = this.submitForm.bind(this)
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    if (this.state.username !== "" && this.state.password !== ""){
+      this.props.onSubmit(this.state)
+    } else {
+      console.log("Fill in both fields")
+     }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.handleUsernameChange} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.handlePasswordChange} value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      charsLeft: this.props.maxChars
+    };
+  }
+
+  updateCharsLeft = (e) => {
+    this.setState({
+      charsLeft: this.props.maxChars - e.target.value.length
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input onChange={this.updateCharsLeft} type="text" />
+        <p>Remaining Characters: {this.state.charsLeft}</p>
       </div>
     );
   }
