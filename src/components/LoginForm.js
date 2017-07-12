b6689c351afc3bea5c328d446d780b276949de63
DanL12186 <danlrcn@hotmail.com>
2017-07-12 18:41:45 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,72 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+      
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+  }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = this.state.username;
+  //   const pass = this.state.password;
+  //
+  //   if (!!name && !!pass) {
+  //     this.props.onSubmit({
+  //       username: name,
+  //       password: pass
+  //     });
+  //   }
+  // };
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const username = this.username;
+    const password = this.password;
+
+    if (!username || !password) {
+      return false;
+    }
+
+    this.props.onSubmit({
+      username,
+      password
+    });
+
+  }
+
+  handleUsernameInput(event) {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  handlePasswordInput(event) {
+     this.setState({
+       password: event.target.value,
+     });
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleUsernameInput}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handlePasswordInput}/>
           </label>
         </div>
         <div>
@@ -29,5 +79,9 @@ class LoginForm extends React.Component {
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit: React.PropTypes.func,
+};
 
 export default LoginForm;
