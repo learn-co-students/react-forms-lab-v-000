6e9f6f0f396019c0f740a8a8cd37dfdbdda35d10
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-18 12:12:26 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,29 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInputChange(event){
+    if(event.target.id === "test-username"){
+      this.setState({
+        username: event.target.value
+      });
+    } else if (event.target.id === "test-password") {
+      this.setState({
+        password: event.target.value
+      })
+    }
+  }
+
+  handleForm(event){
+    event.preventDefault();
+    if (this.state.username.length > 0 && this.state.password.length > 0){
+      this.props.onSubmit(this.state.username, this.state.password);
+    }
   }
 
   render() {
@@ -13,17 +35,17 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleInputChange.bind(this)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handleInputChange.bind(this)}/>
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onClick={this.handleForm.bind(this)}>Log in</button>
         </div>
       </form>
     );
