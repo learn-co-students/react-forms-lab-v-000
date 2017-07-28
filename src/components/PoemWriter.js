bb5cda3012ac7acab28e084ff6bfa9f22d0bddeb
Zenia <zeniavilla@hotmail.com>
2017-07-27 17:44:54 -0700

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,10 +1,49 @@
 import React from 'react';
 
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textValue: '',
+      errorVisible: true,
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      textValue: event.target.value,
+    });
+
+    var textLines = event.target.value.split("\n");
+
+    if (textLines.length === 3) {
+      var firstLineWords = textLines[0].split(" ").remove('');
+      var secondLineWords = textLines[1].split(" ").remove('');
+      var thirdLineWords = textLines[2].split(" ").remove('');
+    }
+
+    if (textLines.length === 3 && firstLineWords.length === 5 && secondLineWords.length === 3 && thirdLineWords.length === 5) {
+        this.setState({
+          errorVisible: false,
+        });
+
+    } else {
+      this.setState({
+        errorVisible: true,
+      });
+    }
   }
 
   render() {
@@ -13,13 +52,20 @@ class PoemWriter extends React.Component {
         <textarea 
           rows="3" 
           cols="60" 
+          value={this.state.textValue}
+          onChange={this.handleInput}
         />
-        <div 
+        {
+          this.state.errorVisible 
+          ? <div 
           id="poem-validation-error" 
           style={{color: 'red'}}
         >
-          This poem is not written in the right structure!
+        "This poem is not written in the right structure!" 
         </div>
+        
+          : null
+        }
       </div>
     );
   }
