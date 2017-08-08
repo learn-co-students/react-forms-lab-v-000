d35d92fe2ed2d9ceabd7daedc8436854bb191a87
Isis Tejeda <isis@isistejeda.com>
2017-08-07 21:04:39 -0500

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,11 +1,54 @@
 import React from 'react';
 
-class PoemWriter extends React.Component {
+function isValidPoem(poem) {
+  var lines = poem.split("\n").filter(line => line)
+  if (lines.length != 3) {
+    return false;
+  }
+
+  var first_line = lines[0].split(" ").filter(line => line).length
+  if (first_line != 5) {
+    return false;
+  }
+
+  var second_line = lines[1].split(" ").filter(line => line).length
+  if (second_line != 3) {
+    return false;
+  }
+
+  var third_line = lines[2].split(" ").filter(line => line).length
+  if (third_line != 5) {
+    return false;
+  }
+
+  return true;
+}
+
+export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+  this.state = {
+        text: '',
+        isValid: false,
+      };
+
+      this.handlePoem = this.handlePoem.bind(this);
+    }
+
+
+  handlePoem(event) {
+    const text = event.target.value;
+
+    if (text) {
+      this.setState({
+        text: text,
+        isValid: isValidPoem(text),
+      });
+    }
   }
+
+
 
   render() {
     return (
@@ -13,16 +56,18 @@ class PoemWriter extends React.Component {
         <textarea 
           rows="3" 
           cols="60" 
+          value={this.state.text}
+          onChange={this.handlePoem}
         />
+      
+        {!this.state.isValid ? 
         <div 
           id="poem-validation-error" 
           style={{color: 'red'}}
         >
           This poem is not written in the right structure!
-        </div>
+        </div> : null}
       </div>
     );
   }
 }
-
-export default PoemWriter;
