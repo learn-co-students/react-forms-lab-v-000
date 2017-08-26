8a2c44f1e4b9035ea65dcd56b795a7321b50bd33
SiamKing <thesiameseelectric@gmail.com>
2017-08-26 11:59:27 -0700

    twitter me this

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,24 +4,58 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: '',
+      validPoem: false
+    };
+  }
+
+  handleTextChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  validPoem = event => {
+    const { text } = this.state;
+    let textArr = text.split('\n');
+
+    if (textArr.length === 3) {
+      if (textArr[0].trim().split(' ').length === 5
+          && textArr[1].trim().split(' ').length === 3
+          && textArr[2].trim().split(' ').length === 5) {
+            return true
+      }
+    }
+    return false
   }
 
   render() {
+    const valid = this.validPoem();
+    console.log(valid)
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
-        />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        ></textarea>
+        { valid ? null : <div id="poem-validation-error" style={{color: 'red'}}>
           This poem is not written in the right structure!
-        </div>
+        </div> }
       </div>
     );
+  }
+}
+
+class PoemError extends React.Component {
+  render() {
+    return (
+      <div id="poem-validation-error" className="poem-error" style={{color: 'red'}}>
+        This poem is not written in the right structure!
+      </div>
+    )
   }
 }
 
