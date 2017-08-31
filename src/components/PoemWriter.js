8bfdc08c769a4311cdf21e609a61d6e6df363905
Bob Habig <rfhab2@gmail.com>
2017-08-31 15:29:55 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,21 +4,37 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
+        <textarea
+          value={this.state.text}
+          onChange={this.handleChange}
+          rows="3"
+          cols="60"
         />
-        <div 
-          id="poem-validation-error" 
+        <div
+          id="poem-validation-error"
           style={{color: 'red'}}
         >
-          This poem is not written in the right structure!
+          {(()=> {
+            if (this.state.text !== '') {
+              null;
+            } else {
+              return 'This poem is not written in the right structure!';
+            }
+          })()}
         </div>
       </div>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      chars: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      chars: event.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.chars}
+        />
+        <p>{this.props.maxChars - this.state.chars.length}</p>
       </div>
     );
   }
 }
+
+TwitterMessage.defaultProps = {
+  maxChars: 140
+};
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number
+};
 
 export default TwitterMessage;
