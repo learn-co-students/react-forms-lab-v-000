db3f6b4f8a1a95629998441b996b797158c5fde8
brownielox <mrsmpython@gmail.com>
2017-08-31 23:31:27 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,29 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: "",
+      error: null
+    };
+  }
+
+  handleChange = (e) => {
+    var lines = e.target.value.split('\n');
+    var error = this.state.error;
+    if(lines.length !== 3) error = 'Poem must be three lines.';
+    else if(lines[0].trim().split(/\W+/).length !== 5 || lines[2].trim().split(/\W+/).length !== 5) error = 'This line must have five words.';
+    else if(lines[1].trim().split(/\W+/).length !== 3)error = 'This line must have three words.';
+    else error = null;
+    this.setState({poem: e.target.value, error: error});
   }
 
   render() {
     return (
       <div>
-        <textarea 
-          rows="3" 
-          cols="60" 
-        />
-        <div 
-          id="poem-validation-error" 
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+        <textarea onChange={this.handleChange} value={this.state.text}
+          rows="3"
+          cols="60" />
+        {(this.state.error) ? <div id="poem-validation-error" style={{color: 'red'}}>{this.state.error}</div>: null}
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: '',
+    };
+  }
+
+  counter = event => {
+    var tweet = event.target.value
+    this.setState({
+      tweet: tweet
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+        value={this.state.tweet}
+        onChange={this.counter}
+        type="text" />
+        <strong>{this.props.maxChars- this.state.tweet.length} characters left</strong>
       </div>
     );
   }
