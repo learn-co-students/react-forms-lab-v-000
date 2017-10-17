172ebb823a09dc5cc8d5d80833ab87d6d48365c7
curlywallst <nnoyes@comcast.net>
2017-10-17 18:13:58 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,45 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: "",
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    if (event.username && event.password) {
+      this.onSubmit(event.username, event.password)}
+      event.preventDefault()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" value="submit">Log in</button>
         </div>
       </form>
     );
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class PoemWriter extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleInputChange = event => {
+    const poemText = parsePoem(event.target.value)
+    console.log(poemText)
+    this.setState({
+      value: event.target.value,
+      withErrors: poemText
+    })
   }
 
   render() {
@@ -13,16 +24,38 @@ class PoemWriter extends React.Component {
         <textarea
           rows="3"
           cols="60"
+          value={this.state.value}
+          onChange={this.handleInputChange}
         />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+        {!this.state.withErrors &&
+          <div
+            id="poem-validation-error"
+            style={{color: 'red'}}
+          >
+            "This poem is not written in the right structure!"
+          </div>
+        }
       </div>
     );
   }
+}
+
+function parsePoem(poemText) {
+  var rows = poemText.split("\n");
+  if (rows.length === 3 && rows[2] !== "") {
+    console.log(rows)
+    if (
+      rows[0].match(/\S+/g).length === 5 &&
+      rows[1].match(/\S+/g).length === 3 &&
+      rows[2].match(/\S+/g).length === 5
+    ) {
+      console.log(rows[0].match(/\S+/g))
+      console.log(rows[1].match(/\S+/g))
+      console.log(rows[2].match(/\S+/g))
+
+      return true
+    } else {return false}
+  } else {return false}
 }
 
 export default PoemWriter;
