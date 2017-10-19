2f94957b711dd30c5558b018ed2784e54402f4b0
Joe <jsbenning4@gmail.com>
2017-10-19 14:56:13 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -10,39 +10,40 @@ class PoemWriter extends React.Component {
   }
 
   checkPoem = (poem) => {
+    const err = document.querySelector('#poem-validation-error');
     let splitPoem, lines;
 
-    String.prototype.trim = function () {
+    String.prototype.trim = function() {
       return this.replace(/\s{2,}/g, ' ');
     };
     
     splitPoem = (poem.split('\n'));
 
-    if (splitPoem[0] && splitPoem[1] && splitPoem[2]) {
+    if (splitPoem.length === 3) {
       lines = splitPoem.map(function(ln) {
-        return ln.trim() //ln.trim();
+        return ln.trim();
       });
+      //console.log(lines[0].split(' ').length);
       if (lines[0].split(' ').length === 5 && lines[1].split(' ').length === 3 && lines[2].split(' ').length === 5) {
-        const err = document.querySelector('#poem-validation-error');
-        err.style.display = 'none';
+        //err.style.visibility= 'hidden';
+        err.innerHTML = ''
+      } else {     
+      //   err.style.visibility = 'visible';
+      err.innerHTML = 'This poem is not written in the right structure!'
       }
-    }
+    } 
   }
 
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
-    });
-
-    if (this.state.poem) {
-      this.checkPoem(this.state.poem)
-    }
+    }, this.checkPoem(this.state.poem));
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" name="poem" value={this.state.poem} onChange={event => this.handleChange(event)} />
+        <textarea rows="3" cols="60" name="poem" value={this.state.poem} onChange={this.handleChange} />
         <div id="poem-validation-error" style={{color: 'red'}} >
           This poem is not written in the right structure!
         </div>
