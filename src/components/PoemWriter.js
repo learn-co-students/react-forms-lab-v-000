942cb0c597fb8ea538b3c0dda0bc748010907386
tpstar <hanlee12@gmail.com>
2017-10-30 08:37:09 -0400

    done

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,22 +4,51 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      isPoemValid: false
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    })
+    const poem = this.state.value.split('\n');
+    if (poem.length === 3) {
+        let arr = [];
+        poem.forEach((line) => {
+          arr.push(line.split(' ').filter((w) => w !== '').length);
+        });
+        if (arr.join('') === '535') {
+          this.setState({isPoemValid: true})
+        }
+    }
+    // console.log('is poem valid?', this.state.isPoemValid)
+  }
+
+  errorMessage = () => {
+    if (!this.state.isPoemValid) {
+      return <div
+        id="poem-validation-error"
+        style={{color: 'red'}}
+      >
+        This poem is not written in the right structure!
+      </div>
+
+    }
   }
 
   render() {
     return (
       <div>
         <textarea
+          onChange={this.handleChange}
+          value={this.state.value}
           rows="3"
           cols="60"
         />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
+          {this.errorMessage()}
       </div>
     );
   }
