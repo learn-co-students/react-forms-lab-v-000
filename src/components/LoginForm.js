6613bc424718da42b939151fed8b386a35a5b43b
James Fernandez <jffernan@comcast.net>
2017-11-08 15:25:12 -0800

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-export default class LoginForm extends React.Component {
+export default class LoginForm extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       username: '',
       password: '',
