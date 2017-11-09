a0b6db8a994d3db2bea7ee1f507aeb00e26592dc
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-08 23:44:28 -0500

    added notes to the Poem Writer

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+//account for spaces
 const wordCount = line => line.split(' ').filter(l => l).length;
 
 const validPoem = poem => {
   if (poem) {
     const poemLines = poem.split('\n').filter(l => l);
+    //poem has 3 lines
     const lineCount = poemLines.length === 3;
+    //first line has five words, second line has three words, third line has five words
     const wordsInLine = wordCount(poemLines[0]) === 5 && wordCount(poemLines[1]) === 3 && wordCount(poemLines[2]) === 5;
     return lineCount && wordsInLine;
   }
@@ -34,12 +37,15 @@ class PoemWriter extends React.Component {
   render() {
     return (
       <div>
+      //make controlled component by adding necessary props to the element
+      //value is saved in the component's state
         <textarea
           rows="3"
           cols="60"
           value={this.state.poem}
           onChange={this.handleChange}
         />
+        //if poem is invalid
         {!this.state.valid && <div id="poem-validation-error" style={{color: 'red'}}>
           This poem is not written in the right structure!
         </div>
