4503eb1959b7d8241c45209210fcca9cb9656990
ruuuh20 <pkim2320@gmail.com>
2017-12-06 15:20:03 -0500

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
+const countWords = line => line.split(' ').filter(l => l).length;
+
+const validPoem = poem => {
+  if (poem) {
+    const lines = poem.split('\n').filter(l => l);
+    const lineCount = lines.length === 3;
+    const wordsAmount = countWords(lines[0]) === 5 && countWords(lines[1]) === 3 && countWords(lines[2]) === 5;
+    return lineCount && wordsAmount;
+
+
+  }
+  return false;
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      validPoem: true
+    };
+  }
+
+  setPoem = event => {
+    const content = event.target.value;
+
+    this.setState({
+      content,
+      isValid: validPoem(content),
+    });
+
   }
 
   render() {
@@ -13,13 +40,17 @@ class PoemWriter extends React.Component {
         <textarea
           rows="3"
           cols="60"
+          value= {this.state.content}
+          onChange={this.setPoem}
         />
+        {!this.state.isValid &&
         <div
           id="poem-validation-error"
           style={{color: 'red'}}
         >
           This poem is not written in the right structure!
         </div>
+        }
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { value: '' };
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,});
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+        value={this.state.value}
+        onChange={this.handleChange} />
+        <span>{this.props.maxChars - this.state.value.length}</span>
       </div>
     );
   }
 }
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number,
+};
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
 
 export default TwitterMessage;
