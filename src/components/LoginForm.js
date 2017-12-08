d8df956062f9353cb5340acdc78f9caddf857a90
Michael Burak <michaelrburak@gmail.com>
2017-12-08 16:43:43 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,47 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUserChange = this.handleUserChange.bind(this)
+    this.handlePWChange = this.handlePWChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleUserChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePWChange(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" value={this.state.username} type="text" onChange={this.handleUserChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" value={this.state.password} type="password" onChange={this.handlePWChange}/>
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,25 +1,54 @@
 import React from 'react';
 
 class PoemWriter extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: '',
+      showError: false
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    })
+    console.log(this.poemValidator(event.target.value))
+  }
+
+  poemValidator(poem){
+    const poemLines = poem.split("\n")
+    if(poemLines.length != 3) {
+      return false
+    }
+    else if(poemLines.map(line => line.split(" ").filter(word => word != '').length != ['5', '3', '5'])){
+      {
+        this.setState({
+          showError: true
+        })
+      }
+  }
   }
 
   render() {
     return (
       <div>
         <textarea
+          value = {this.state.value}
+          onChange = {this.handleChange}
           rows="3"
           cols="60"
         />
-        <div
+        {!this.state.showError &&
+          <div
           id="poem-validation-error"
           style={{color: 'red'}}
         >
           This poem is not written in the right structure!
         </div>
+      }
       </div>
     );
   }
