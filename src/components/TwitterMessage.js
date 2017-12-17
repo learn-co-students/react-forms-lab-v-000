52b8809c0f5646e6104dc49fb6bed0e3dc28b7a0
Sophia Hubeny <shhubeny@gmail.com>
2017-12-17 05:11:38 +0000

    saving for tomorrow

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value : ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value}/>
       </div>
     );
   }
+}
+
+TwitterMessage.propTypes = {
+  maxChars : PropTypes.number
 }
 
 export default TwitterMessage;
