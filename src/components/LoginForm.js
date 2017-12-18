1fe645ebc0fa1ef289f24d4cbdb7385723d8675b
Jim Stricker <jmstricker93@gmail.com>
2017-12-18 18:47:12 -0500

    login form tests passing

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      usernameValue: '',
+      passwordValue:''
+    };
+  }
+
+  handleUsernameValueChange = (event) => {
+    this.setState({
+      ...this.state,
+      usernameValue: event.target.value
+    })
+  }
+
+  handlePasswordValueChange = (event) => {
+    this.setState({
+      ...this.state,
+      passwordValue: event.target.value
+    })
+  }
+
+  handleSubmission = (event) => {
+    event.preventDefault();
+    return this.props.onSubmit
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmission}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.handleUsernameValueChange} value={this.state.usernameValue} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.handlePasswordValueChange} value={this.state.passwordValue} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button onClick={this.handleSubmission} type="submit">Log in</button>
         </div>
       </form>
     );
