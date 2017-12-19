b963be32ba57c39aaa24bbdeafb9134255303180
Jim Stricker <jmstricker93@gmail.com>
2017-12-19 13:38:11 -0500

    validation method stubbed out, works on simpler example

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,19 +4,41 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: "",
+      errorDisplay: ""
+    };
   }
+
+  updateContent = (e) => {
+    this.setState({
+      content: e.target.value
+    }, this.validatePoem)
+
+  }
+
+  validatePoem = () => {
+    let poem = this.state.content
+    if (poem.length > 5){
+      this.state.errorDisplay = "none";
+    } else {
+      this.state.errorDisplay = "";
+    }
+    this.setState(this.state)
+  };
 
   render() {
     return (
       <div>
         <textarea
+          value = {this.state.content}
           rows="3"
           cols="60"
+          onChange={this.updateContent}
         />
         <div
           id="poem-validation-error"
-          style={{color: 'red'}}
+          style={{color: 'red', display: this.state.errorDisplay}}
         >
           This poem is not written in the right structure!
         </div>
