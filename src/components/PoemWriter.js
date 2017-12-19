1860de4562388e5150abb862a1d89f23360652bd
psnider30 <pbsnider@gmail.com>
2017-12-19 12:30:10 -0800

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
+const linesThree = (poem) => {
+  return poem.split(/\n/).length === 3;
+}
+const firstIsFive = (poem) => {
+  return poem.split(/\n/)[0].trim().split(' ').length === 5;
+}
+const secondIsThree = (poem) => {
+  return poem.split(/\n/)[1].trim().split(' ').length === 3;
+}
+const thirdIsFive = (poem) => {
+  return poem.split(/\n/)[2].trim().split(' ').length === 5;
+}
+// const wordCount = this.state.content.split(/\n/).join(' ').split(' ').length;
+const isValidPoem = (poem) => {
+  if (linesThree(poem) && firstIsFive(poem) && secondIsThree(poem) && thirdIsFive(poem)) {
+    return true;
+  }
+  return false;
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      isValid: false,
+    };
+  }
+
+  updatePoem = (event) => {
+    const content = event.target.value;
+    this.setState( {
+      content: content,
+      isValid: isValidPoem(content)
+    })
   }
 
   render() {
@@ -13,13 +44,18 @@ class PoemWriter extends React.Component {
         <textarea
           rows="3"
           cols="60"
+          value={this.state.content}
+          onChange={this.updatePoem}
         />
+
+      {!this.state.isValid &&
         <div
           id="poem-validation-error"
           style={{color: 'red'}}
         >
           This poem is not written in the right structure!
         </div>
+      }
       </div>
     );
   }
