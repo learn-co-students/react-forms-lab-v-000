a9ba64eaf26534a51eb42105dd4f4ea466560dfb
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-28 13:27:41 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+      username: event.target.value,
+      password: event.target.value,
+    });
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={data => { data.preventDefault(); }}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username"
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { maxChars: 140 };
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+      maxChars: 140 - event.target.value.length
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <p>
+          {this.state.maxChars} characters are still available for you message
+        </p>
       </div>
     );
   }
