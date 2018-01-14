05ecb7ff4963b1500870a5e81f07c1a373b6c1c2
Art Rodriguez <art.rodriguez@live.com>
2018-01-14 01:19:10 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,43 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value })
+    // console.log(event)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        {this.props.maxChars - this.state.value.length}
+        {/* {console.log(this.state.value)} */}
       </div>
     );
   }
+}
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
+
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number,
 }
 
 export default TwitterMessage;
--- test/TwitterMessage-test.js
+++ test/TwitterMessage-test.js
@@ -2,6 +2,11 @@ import React from "react";
 import { expect } from "chai";
 import { shallow } from "enzyme";
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 import TwitterMessage from "../src/components/TwitterMessage";
 
 describe("<TwitterMessage />", () => {
