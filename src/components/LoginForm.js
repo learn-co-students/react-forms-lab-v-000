38ed01decabfa7f250c841bcc9ed4cf48ad604f2
jake <jtb1137@gmail.com>
2018-01-17 10:32:00 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,32 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      username: event.target.value,
+      password: event.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={data => {data.preventDefault()}}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
@@ -31,3 +41,19 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+
+/*
+This component takes one prop, onSubmit, which is a function that gets called when the form is being submitted.
+
+You'll find two inputs in this component: <input type="text"> and <input type="password">. Make this a controlled component by 
+  adding the necessary props to these inputs. Their values should be saved in the component's state.
+
+An example of an input would look like: js <input id="test-username" type="text" name="username" value={this.state.username} 
+  onChange={this.handleInputChange} />
+
+Remember that you can retrieve the input name and value of an event.target from the JS event.
+
+Add the necessary event handler to the <form> element in order to call the onSubmit callback prop.
+
+The onSubmit callback prop should only be called if both fields are filled in (with any value).
+*/
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,47 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  setMessage = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message} onChange={this.setMessage}/>
+        <p>{this.props.maxChar - this.state.message.length}</p>
       </div>
     );
   }
 }
 
+TwitterMessage.propTypes = {
+  maxChar: PropTypes.number
+}
+
+TwitterMessage.defaultProps = {
+  maxChar: 140
+}
+
 export default TwitterMessage;
+
+
+/*
+Open the components/TwitterMessage.js file.
+This component takes one prop, maxChars, which is a number representing the maximum amount of characters a message can have.
+You'll find an <input type="text"> in this component. Make this a controlled component by adding the necessary props to the <input> element. Its value should be saved in the component's state.
+Show the remaining characters in the component. It doesn't matter how you render it, as long as the number is correct. No need to guard against input that is too long — you can let the counter reach negative values.
+
+*/
