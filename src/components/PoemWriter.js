3dce1eec4f87f78a6031f27f49de9aa2a9af0143
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-19 18:46:09 -0500

    refactored validPoem()

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -12,20 +12,13 @@ class PoemWriter extends React.Component {
 
   validPoem = () => {
     const poem = this.state.value.split('\n');
+    const poemFormattCorrect = (
+      poem[0].trim().split(" ").length === 5 &&
+      poem[1].trim().split(" ").length === 3 &&
+      poem[2].trim().split(" ").length === 5
+    )
 
-    if( poem.length === 3 ) {
-
-      if ( poem[0].trim().split(" ").length === 5 &&
-           poem[1].trim().split(" ").length === 3 &&
-           poem[2].trim().split(" ").length === 5 ) {
-             return true;
-           } else {
-             return false;
-           }
-
-    } else {
-      return false;
-    }
+    return poem.length === 3 && poemFormattCorrect;
 
   }
 
