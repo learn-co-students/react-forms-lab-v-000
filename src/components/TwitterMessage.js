ff3ac9aac1e9551787539ae3a19ee136c77b3ebc
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-30 21:34:30 -0800

    twitter and login form done

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,33 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      remainingChars: 140,
+    };
+    //again, line 11 only needed for debugging.
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+    // debugger;
+    this.setState({
+      value: event.target.value,
+      remainingChars: this.props.maxChars - event.target.value.length,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
+        <p>Max chars: {this.props.maxChars} </p>
+        <p>You have {this.state.remainingChars} char left. </p>
       </div>
     );
   }
