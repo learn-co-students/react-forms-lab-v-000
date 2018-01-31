bdcd8fb50bb55c2e5e3197da5feaecdcdaa08325
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-31 00:44:22 -0800

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -10,6 +10,8 @@ class PoemWriter extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.checkPoemValidity = this.checkPoemValidity.bind(this);
+    this.stripSpaces = this.stripSpaces.bind(this);
   }
 
   stripSpaces(poemRow) {
@@ -23,20 +25,27 @@ class PoemWriter extends React.Component {
     return words;
   }
 
+  checkPoemValidity(poem) {
+    const poemArray = poem.split("\n");
+    const poemArrayStripped = poemArray.map(
+      line => this.stripSpaces(line)
+    )
+    return (poemArrayStripped.length === 3 && poemArrayStripped[0].length === 5 && poemArrayStripped[1].length === 3 && poemArrayStripped[2].length === 5 )
+  }
+
   handleChange = (event) => {
-    // debugger;
+    const poemValidity = this.checkPoemValidity(event.target.value)
     this.setState({
       value: event.target.value,
-      numOfRows: event.target.value.split("\n").length,
+      correctPoem: poemValidity,
     })
   }
 
-// text.split("\n")
   render() {
     return (
       <div>
         <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange} />
-        { this.state.numOfRows !== 3 &&
+        { !this.state.correctPoem &&
             <div id="poem-validation-error" style={{ color: "red" }}>
               This poem is not written in the right structure!
             </div>
