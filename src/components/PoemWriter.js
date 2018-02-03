7a67d8b03e2c9a9c8d672b7a4fccb1ef20f6c6ef
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-03 14:57:03 -0500

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,45 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      isValid: false
+    };
+  }
+
+  handleChange = event => {
+      let validPoem = false;
+      const poemLines = event.target.value.split("\n").map(line => line.trim());
+      const isRightAmountOfLines = poemLines.length === 3;
+      if (event.target.value && isRightAmountOfLines) {
+        if (poemLines[0].split(" ").length === 5 && poemLines[1].split(" ").length === 3 && poemLines[2].split(" ").length === 5) {
+          validPoem = true;
+        }
+      }
+
+      this.setState({
+      value: event.target.value,
+      isValid: validPoem
+      });
   }
 
   render() {
+
+    const isValid = this.state.isValid;
+
+    let error = null;
+    if (isValid) {
+      error = <div/>;
+    } else {
+      error = <div id="poem-validation-error" style={{ color: "red" }}>
+          This poem is not written in the right structure!
+        </div>;
+    }
+
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange}/>
+        {error}
       </div>
     );
   }
