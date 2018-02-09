37b1640a511d2fbd15f7ce494489ee4a2a74819b
Soumya Veer <veer.soumya@gmail.com>
2018-02-09 22:29:11 +0000

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,19 +4,71 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      verses: "",
+    };
+  };
+
+  setPoem = event => {
+     this.setState({
+       verses: event.target.value
+     });
+   };
+
+  validatePoem = () => {
+    const poem = this.state.verses
+    const verses = poem.split("\n").map(line => line.trim());
+    console.log(verses)
+    const isValidPoemLength = (verses.length === 3)
+    console.log("Validate Poems Length:", isValidPoemLength)
+
+    if (poem && isValidPoemLength) {
+      const isValidVersesLength = (verses[0].split(" ").length === 5 && verses[1].split(" ").length === 3 && verses[2].split(" ").length === 5)
+      console.log("Verse 1:",  verses[0].split(" "))
+      console.log("Validate lines length:", isValidVersesLength)
+      if (isValidVersesLength){
+      return true
+      } else {
+        return false
+      }
+    }
+    else {
+      return false
+    }
   }
 
-  render() {
-    return (
-      <div>
-        <textarea rows="3" cols="60" />
+  isValid = () => {
+    const isvalidPoem = this.validatePoem()
+    if(!isvalidPoem) {
+      return (
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
+      )
+    } else {
+     return null
+      }
+    }
+
+
+  render(){
+    return (
+      <div>
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.verses}
+          onChange={this.setPoem}
+        />
+        <div>{this.isValid()}</div>
       </div>
     );
   }
 }
 
+// {!this.validatePoem() ? (
+//   <div id="poem-validation-error" style={{ color: "red" }}>
+//     This poem is not written in the right structure!
+//   </div>
+// ) : null}
 export default PoemWriter;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,43 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
+  }
+
+  setTweet = event => {
+    this.setState({
+      tweet: event.target.value
+    })
+  }
+
+  charactersLeft = () => {
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.setTweet} />
+        <div>{this.props.maxChars - this.state.tweet.length}</div>
       </div>
     );
   }
 }
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number
+};
+
+TwitterMessage.defaultProps = {
+  maxChars: 140
+};
+
 
 export default TwitterMessage;
