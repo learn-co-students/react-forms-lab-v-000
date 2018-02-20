4c6a2bc9d16c2c70535e39829af2bcca7907f5b8
github username <github email address>
2018-02-19 22:24:26 -0800

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,45 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      valid: false,
+    };
   }
+
+  checkCorrectFields = () => {
+    const rows = this.state.value.split("\n")
+    return ((rows.length === 3) &&
+      (rows[0].split(" ").filter(word => word).length === 5) &&
+      (rows[1].split(" ").filter(word => word).length === 3) &&
+      (rows[2].split(" ").filter(word => word).length === 5)
+    )
+  }
+
+  handleInput = event => {
+    this.setState({
+      value: event.target.value,
+    }, () => {
+        if (this.checkCorrectFields()) {
+          this.setState({
+            valid: true
+          })
+        } else {
+          this.setState({
+            valid: false
+          })
+        }
+    });
+  }
+
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleInput}/>
+        {this.state.valid ? null : (<div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
-        </div>
+        </div>)}
       </div>
     );
   }
