b05f58536685c1301d57a9b966ed643c1ceefba5
Janusz Szubert <theinvalidnonce@gmail.com>
2018-03-15 23:31:18 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,50 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+  
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+  
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+  
+  onSubmit = (e) => {
+    e.preventDefault()
   }
 
   render() {
     return (
-      <form>
+      <form
+        onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input 
+            id="test-username" 
+            type="text" 
+            value={this.state.username}
+            onChange={this.handleUsernameChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input 
+            id="test-password" 
+            type="password" 
+            value={this.state.password}
+            onChange={this.handlePasswordChange}/>
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,58 @@
 import React from "react";
 
+  // Are const assignments not allowed in the component logic?
+
+  // Check the poem structure
+  const isPoemValid = poem => {
+    // Split the poem on newline, trim each line for whitespace
+    const poemLines = poem.split('\n').map(line => line.trim())
+    // Check the amount of lines
+    const isRightAmountOfLines = poemLines.length === 3
+    
+    // if the poem exists and has 3 lines
+    if (poem && isRightAmountOfLines) {
+      return (
+        // Check if each line is the exact amount
+        poemLines[0].split(" ").length === 5 &&
+        poemLines[1].split(" ").length === 3 &&
+        poemLines[2].split(" ").length === 5
+        )
+    }
+    else {
+      return false
+    }
+  }
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      valid: true
+    };
+  }
+  
+  handlePoemChange = (e) => {
+    this.setState({
+      content: e.target.value,
+      valid: isPoemValid(e.target.value)
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea 
+        rows="3" 
+        cols="60"
+        value={this.state.content}
+        onChange={this.handlePoemChange}/>
+        {(this.state.valid) ? null :
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
+        }
       </div>
     );
   }
