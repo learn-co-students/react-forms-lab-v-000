fd9d35f664a62f6d0e92537f4c815e4764bdd11b
Hannah Carnes <hecarnes@gmail.com>
2018-03-20 11:04:03 -0400

    validate poem structure

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -15,13 +15,25 @@ class PoemWriter extends React.Component {
     this.setState({ value: event.target.value });
   }
 
+  validPoem() {
+    const lines = this.state.value.split("\n")
+    if (lines.length === 3) {
+      var [one, two, three] = lines
+      return one.trim().split(" ").length === 5 && two.trim().split(" ").length === 3 && three.trim().split(" ").length === 5
+    } else {
+      return false
+    }
+  }
+
   render() {
     return (
       <div>
         <textarea value={this.state.value} onChange={this.handleChange} rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        {!this.validPoem() &&
+          <div id="poem-validation-error" style={{ color: "red" }}>
+            This poem is not written in the right structure!
+          </div>
+        }
       </div>
     );
   }
