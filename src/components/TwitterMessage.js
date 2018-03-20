eae1b128af3003460dee20999f8366a5d6b6937f
Hannah Carnes <hecarnes@gmail.com>
2018-03-20 11:08:12 -0400

    save twitter message input value in state

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       value: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" value={this.state.value} />
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
       </div>
     );
   }
