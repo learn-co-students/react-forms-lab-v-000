e15b2dcc13598d118fd7c8b78f1cb5c74af29046
Oski <github email address>
2018-03-21 05:50:45 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,29 @@
 import React from "react";
+//read more about proptypes here: https://reactjs.org/docs/typechecking-with-proptypes.html
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = { 
+      message: "" 
+    };
+
+    this.setMessage = this.setMessage.bind(this)
+  }
+  
+  setMessage(e){
+    this.setState({
+      message: e.target.value,
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message} onChange={this.setMessage}/>
+        <span>{this.props.maxChars - this.state.message.length}</span>
       </div>
     );
   }
