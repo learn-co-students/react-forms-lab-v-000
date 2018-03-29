0d1dedccf4939e68d5dc4057838dbc1ca2ca64e1
Heather You <heatherhyou@gmail.com>
2018-03-29 15:00:19 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,54 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInputUsername = (event) => {
+    this.setState({username: event.target.value})
+  }
+
+  handleInputPassword = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  checkCredentials = (event) => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      this.props.onSubmit
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.checkCredentials}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input
+              id="test-username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInputUsername}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input
+              id="test-password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInputPassword}
+            />
           </label>
         </div>
         <div>
@@ -28,6 +57,11 @@ class LoginForm extends React.Component {
       </form>
     );
   }
+}
+
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm;
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,56 @@
 import React from "react";
 
+const isValid = (poem) => {
+  let poemLines = poem.split('\n').map(line => line.trim());
+
+  let firstLine = (poemLines[0]);
+  let secondLine = (poemLines[1]);
+  let thirdLine = (poemLines[2]);
+
+  if (poemLines.length === 3) {
+    return (
+      firstLine.split(" ").length === 5 &&
+      secondLine.split(" ").length === 3 &&
+      thirdLine.split(" ").length === 5
+    );
+  } else {
+    return false;
+  }
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: '',
+      validity: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const poem = event.target.value;
+
+    this.setState({
+      poem,
+      validity: isValid(poem),
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea
+          rows="3"
+          cols="60"
+          value={this.state.poem}
+          onChange={this.handleChange}
+        />
+        {!this.state.validity ? (
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
+        ) : null }
       </div>
     );
   }
