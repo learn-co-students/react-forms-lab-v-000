7eb5cc6268a54d3d0dfacdbca384e5cef572a38c
Christian Egglin <cegglin@conncoll.edu>
2018-04-04 10:23:21 +0000

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,37 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poemBody: '',
+      isValid: false
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      poemBody: event.target.value,
+      isValid: this.isPoemValid(event.target.value)
+    })
+  }
+
+  isPoemValid = (content) => {
+    const contentByLine = content.split("\n").map(line => line.trim())
+    if (contentByLine.length === 3 && contentByLine[0].split(" ").length === 5 && contentByLine[1].split(" ").length === 3 && contentByLine[2].split(" ").length === 5) {
+      return true
+    } else {
+      return false
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea value={this.state.poemBody} onChange={this.handleChange} rows="3" cols="60" />
+        {this.state.isValid ? <div id="poem-congratulations" style={{ color: "blue" }}>
+          A masterpiece of a poem, truly a work of art!</div>
+          : <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
-        </div>
+        </div>}
       </div>
     );
   }
