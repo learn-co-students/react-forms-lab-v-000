2de20a3a25a31f1396cf8ec7ef99dc99826b5179
Alexander Urizar <urizaraj@gmail.com>
2018-04-14 09:43:02 -0400

    Complete lab

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,18 +4,53 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      valid: false
+    };
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea rows="3" cols="60" onChange={this.handleChange} value={this.state.value} />
+        { this.error() }
+      </div>
+    );
+  }
+
+  handleChange = event => {
+    const value = event.target.value
+
+    this.setState({
+      value: value,
+      valid: this.validPoem(value.split('\n'))
+    })
+  }
+
+  error() {
+    if (!this.state.valid) {
+      return (
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
-      </div>
-    );
+      )
+    }
+  }
+
+  numWords(sentence, length) {
+    const words = sentence.trim().split(' ').length
+    return (words === length)
+  }
+  
+  validPoem(sentences) {
+    switch (false) {
+      case (sentences.length === 3): return false
+      case this.numWords(sentences[0], 5): return false
+      case this.numWords(sentences[1], 3): return false
+      case this.numWords(sentences[2], 5): return false
+    }
+    return true
   }
 }
 
