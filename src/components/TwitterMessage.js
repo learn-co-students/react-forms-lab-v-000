7f03511f28723f57de384598bb96c7b34bacfd82
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-19 02:56:28 +0000

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,29 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: '',
+      counter: this.props.maxChars
+    };
   }
 
+  handleTyping = event => {
+    this.setState({
+      value: event.target.value,
+      counter: this.props.maxChars - event.target.value.length
+    });
+     
+  }
+ 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleTyping}/>
+        <p>Counter: {this.state.counter}</p>
       </div>
     );
   }
