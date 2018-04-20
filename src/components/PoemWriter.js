6a70ff199fffc5d84a6f35688b9732abc52a225d
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-20 00:00:40 +0000

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,7 +4,7 @@ import React from "react";
 const validatePoem = (input) => {
     let poemArray = input.split("\n");
     if (poemArray.length === 3) {
-      if (poemArray[0].split(' ').length === 5 && poemArray[1].split(' ').length === 3 && poemArray[2].split(' ').length ===5 ) {
+      if (poemArray[0].trim().split(' ').length === 5 && poemArray[1].trim().split(' ').length === 3 && poemArray[2].trim().split(' ').length ===5 ) {
         return true
       } else {
         return false
