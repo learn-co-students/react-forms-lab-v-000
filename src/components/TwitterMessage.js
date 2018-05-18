f97d847cc3aba8882645d1c847bad35ac0e019ec
chrisjamesr <chrisjames.r@gmail.com>
2018-05-17 22:43:06 -0400

    complete twittermessage callback and rendering

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { message: ''};
+  }
+  charLimit= (event) => {
+    this.setState({
+      message: event.target.value,
+    }, ()=> console.log(this.state.message))
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+               value={this.state.message}
+               onChange={this.charLimit} 
+        />
+        <label>{this.props.maxChars-this.state.message.length}</label>
       </div>
     );
   }
