e31ccb4c1001d8e196aacf308d4bed0eb179a9cb
shravnk <brendan.ode4@gmail.com>
2018-06-04 20:19:31 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,40 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      hasError: true
+    };
   }
+
+    handleChange = event => {
+      this.setState({
+        value: event.target.value,
+        hasError: this.checkError(event.target.value)
+      });
+    }
+
+    checkError = value => {
+      const splitByLines = value.split("\n").map(line => line.trim())
+      const checkNumberOfLines = splitByLines.length === 3
+      if (value && checkNumberOfLines) {
+        return (
+          splitByLines[0].split(" ").length === 5 &&
+          splitByLines[1].split(" ").length === 3 &&
+          splitByLines[2].split(" ").length === 5
+      )
+      }
+    }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" onChange={this.handleChange}  />
+        {!this.state.hasError ? (
+          <div id="poem-validation-error" style={{ color: "red" }}>
+            This poem is not written in the right structure!
+          </div>
+        ) : null}
       </div>
     );
   }
