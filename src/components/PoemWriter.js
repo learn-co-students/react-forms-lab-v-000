90fec3082804b335805ddaf1de05e15b0163cf63
jendobes <jdobak1@gmail.com>
2018-06-05 21:56:27 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,19 +1,42 @@
 import React from "react";
 
+const isValidPoem = poem => {
+  const poemLines = poem.split("\n").map(line => line.trim());
+  const isRightAmountOfLines = poemLines.length === 3;
+  if (poem && isRightAmountOfLines) {
+    return (
+      poemLines[0].split(" ").length === 5 &&
+      poemLines[1].split(" ").length === 3 &&
+      poemLines[2].split(" ").length === 5
+    );
+  } else {
+    return false;
+  }
+};
+
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      valid: true
+    };
   }
+
+renderChange = event => {
+  const poem = event.target.value
+  this.setState({poem, valid: isValidPoem(poem)})
+}
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea value={this.state.value} onChange={this.renderChange} rows="3" cols="60" />
+        {!this.state.valid ? (<div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
-        </div>
+        </div>) : null}
       </div>
     );
   }
