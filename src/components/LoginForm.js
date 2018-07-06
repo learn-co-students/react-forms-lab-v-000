b5acfd5af2717f72e3c5642374b1404469fef56d
scliff6262 <sclifford6262@gmail.com>
2018-07-05 21:48:12 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  changeUsername = e => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  changePassword = e => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  userLogin = e => {
+    e.preventDefault()
+    const username = this.state.username
+    const pass = this.state.password
+    debugger;
+    if (username !== "" && pass !== ""){
+      this.props.onSubmit(username, pass)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.userLogin}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.changeUsername}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.changePassword}/>
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,15 +4,39 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      isError: true
+    };
   }
 
+  properPoem(str) {
+    const noWhiteSpace = str.replace(/\n/g, " ").split(" ").map(function(el){return el.trim()})
+    const justWords = noWhiteSpace.filter(function(el){return el !== ""})
+    if (justWords.length === 13){
+      this.setState({isError: false})
+    } else {
+      this.setState({isError: true})
+    }
+  }
+
+  handleChange = e => {
+    this.properPoem(e.target.value)
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+
   render() {
+    console.log(this.state.isError)
+    let style = this.state.isError ? {color: "red"} : {display: "none"}
+
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange}/>
+        <div id="poem-validation-error" style={style}>
+          This poem is not written in the right structure
         </div>
       </div>
     );
