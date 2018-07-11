3d037944dc4808d3348b4a84477232b067611ec5
btcline04 <btcline04@gmail.com>
2018-07-11 15:39:38 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,50 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleOnSubmit = this.handleOnSubmit.bind(this)
   }
 
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    if( !!this.state.username.trim() && !!this.state.password.trim() ){
+      this.props.onSubmit(this.state)
+    }
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleOnSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username"
+            name="username" 
+            type="text" 
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password"
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleInputChange} 
+            />
           </label>
         </div>
         <div>
@@ -28,6 +56,10 @@ class LoginForm extends React.Component {
       </form>
     );
   }
+}
+
+LoginForm.propTypes = {
+
 }
 
 export default LoginForm;
