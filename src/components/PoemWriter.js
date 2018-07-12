bc5f290d31c86c96bbe3e8e15b0bbbf9784cdeb4
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-07-12 02:25:26 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,13 +4,24 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: ''
+    };
+  }
+
+  // class method? or otuside function? isValid = ()
+
+  handleChange = (event) => {
+    this.setState({
+      poem: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea rows="3" cols="60" value={this.state.poem} onChange={this.handleChange} />
+         {/* if poem is valid show else null */}
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
