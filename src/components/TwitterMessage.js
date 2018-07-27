81adc8fe49367dd6e3a302a4a486a3085c882457
catalanowebdevelopment <catalano.work@gmail.com>
2018-07-27 12:19:29 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
     this.state = {};
+  }
+    
+  TwitterMessage.propTypes = {
+      maxChars: (props, propName) => {
+          const maxChars = props[propName];
+          
+          if (maxChars === undefined) {
+              return new Error('Your message must have content to post!')
+          }
+          
+          const isValidLength = maxChars > 0 && maxChars < 140;
+          
+          if (!isValidLength) {
+              return new Error('You post must be greater than 0 characters and less than 140 characters.')
+          }  
+      }
   }
 
   render() {
