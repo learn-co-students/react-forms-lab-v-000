b12ab241ca31e20ee7d4d563cd0844a572000809
Michelle Olivieri <michelleo72@gmail.com>
2018-07-30 11:11:54 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,44 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      nameValue: '',
+      pwdValue: ''
+    };
+  }
+
+  handleNameChange = event => {
+    this.setState({
+      nameValue: event.target.value
+    })
+  }
+
+  handlePwdChange = event => {
+    this.setState({
+      pwdValue: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    if (this.state.nameValue !== '' && this.state.pwdValue !== '') {
+      this.props.onSubmit()
+    }
+    event.preventDefault()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.nameValue} onChange={this.handleNameChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.pwdValue} onChange={this.handlePwdChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      msgValue: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      msgValue: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.msgValue} onChange={this.handleChange} />
+        Characters Remaining: {this.props.maxChars - this.state.msgValue.length}
       </div>
     );
   }
