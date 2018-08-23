e23ee5aa5f3e66f20808ef8f8928b367c7d9f396
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-23 16:39:36 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
   submitData = event => {
     event.preventDefault();
     if (this.state.username && this.state.password) {
-      this.props.onSubmit(this.state.username, this.state.password);
+      this.props.onSubmit(this.state);
     }
   }
 
