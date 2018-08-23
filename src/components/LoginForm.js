8d12d5fcc60fc69adf279ba74d7907813d9efb63
fonzmp <fonzmp@gmail.com[color]>
2018-08-22 22:49:24 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  preventDefault = event => {
+    event.preventDefault();
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.preventDefault(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              onChange={this.handleInputChange}
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={this.handleInputChange}
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  absorbMessage = event => {
+    this.setState({
+      message: event.target.value
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          max={this.props.maxChars}
+          onChange={this.absorbMessage}
+          value={this.state.message}
+        />
+        <label>{this.props.maxChars - this.state.message.length}</label>
       </div>
     );
   }
