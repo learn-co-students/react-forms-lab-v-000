19710480240d24563bb7d4e2e32d8d0c159c5fce
Kiveo <mavericktesini@gmail.com>
2018-09-07 16:13:26 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,50 @@
-import React from "react";
+// This component takes one prop: onSubmit which is a function — this function is called when the form is being submitted. 
+// By default, this function is currently just includes console.log that will allow you to see if the form is functioning correctly when working in your browser.
 
+// You'll find two inputs in this component: <input type="text"> and <input type="password">. 
+// Make this a controlled component by adding the necessary attributes to these inputs. 
+// The input values should be saved to the component's state, again on every change.
+import React from "react";
 class LoginForm extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  handleUser = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePass = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // this.sendFormDataSomewhere(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleUser} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handlePass} value={this.state.password} />
           </label>
         </div>
         <div>
