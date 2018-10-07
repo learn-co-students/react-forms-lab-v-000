91bb47c6a801a483e9e01a2ce090c39eff7c8b4b
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-06 21:01:37 -0400

    Implement TwitterMessage component to spec. All related tests passing.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
+  }
+
+  handleChange = (event) => {
+      this.setState({
+        tweet: event.target.value
+      })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.handleChange} />
+        <p>Characters left: {this.props.maxChars - this.state.tweet.length}</p>
       </div>
     );
   }
