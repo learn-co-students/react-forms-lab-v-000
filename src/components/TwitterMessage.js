02ff37d1e28356321914faf50ec53709cb4fb1a4
Michele Conway <mconway890@gmail.com>
2018-10-16 12:38:10 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
 
   render() {
+    const currentCount = this.props.maxChars-this.state.message.length
+
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={e => this.setState({message: e.target.value})} value={this.state.message}/>
+          <p>Characters Left: {currentCount > 0 ? currentCount : "Max character limit reached."}</p>
       </div>
     );
   }
