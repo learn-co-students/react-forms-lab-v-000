b10379fb7620911d7e0b723da048751a45b7724d
Jrodr4544 <maskdbandit@gmail.com>
2018-10-22 15:41:40 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
+
   login = ({ username, password }) => {
 	  console.log(`Logging in ${username} with password ${password}`);
   };
