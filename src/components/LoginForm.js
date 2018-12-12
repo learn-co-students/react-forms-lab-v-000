53a6cb5e37a4c49c3b3c39e4b2af5bb4eced50eb
asabogal <asabogal.git@gmail.com>
2018-12-11 19:32:59 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,24 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (this.state.username !== '' && this.state.password !== '') {
+      return this.props.onSubmit()
+    }
+  
   }
 
   render() {
@@ -13,17 +30,23 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" 
+            value={this.state.username}
+            onChange={this.handleChange}
+             />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+            value={this.state.password}
+            onChange={this.handleChange}
+            />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onClick={this.handleSubmit}>Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+        name="tweet"
+        value={this.state.tweet}
+        onChange={event => this.setState({tweet: event.target.value}, console.log(this.state))}/>
+        
+        <p>Characters left: <strong>{this.props.maxChars-this.state.tweet.length}</strong></p>
       </div>
     );
   }
