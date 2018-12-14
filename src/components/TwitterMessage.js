4a70481b27a58b130d227f6d40bdfaa2b1ca678c
MiriamPeskowitz <miriam5000@gmail.com>
2018-12-13 19:37:40 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,43 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
+
+  // handleChange = event => {
+  //   this.setState({
+  //     message: event.target.value
+  //   })
+    
+  // }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input 
+            type="text" 
+            value={this.state.message}
+            onChange={event => { this.setState({message: event.target.value}) }} />
+              {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+//         {this.props.maxChars-this.state.message.length}
+  // countCharacters = () => {
+  //     console.log(this.props.maxChars - this.state.message.length)
+  // }
+
+// onChange={event => this.setState({message: event.target.value})} value={this.state.message}/>
+//         {this.props.maxChars-this.state.message.length}
+//because callback prop is needed to send data back up to parent. That's why not separate functions 
+
+// maxChars: 140
+
+// charCount = 140 - value.count
