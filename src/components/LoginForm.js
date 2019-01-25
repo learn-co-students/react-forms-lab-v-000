91bceab4f9d5d1cb24406cfd11c25c452a448cf5
Jim <jimlotruglio@yahoo.com>
2019-01-24 18:44:36 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,45 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "username",
+      password: "password"
+
+    };
   }
+
+  handleLogin = (event)=>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+handleSubmit = (event)=>{
+  event.preventDefault()
+  let username = this.state.username
+  let password = this.state.password
+  if(username !== "" && password !== ""){
+
+    this.props.onSubmit({username: username, password: password})
+  }
+}
+
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleLogin} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleLogin} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: "this content",
+      characters: 140
+    };
+
+  }
+
+  changeContent = (event)=>{
+    this.setState({
+      content: event.target.value,
+      characters: this.props.maxChars - event.target.value.length
+    })
+    console.log(this.state.characters)
+
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.content} onChange={this.changeContent}/>
+        <br/>Characters remaining: {this.state.characters}
       </div>
     );
   }
