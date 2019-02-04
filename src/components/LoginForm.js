caf6778f0db6a6643bbdcd3874afa94fe4afe428
Vibraniumforge <gregm9999@gmail.com>
2019-02-03 19:55:27 -0800

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,33 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:'',
+      password:''
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+  
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={ event => this.handleSubmit(event) }>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
+  }
+
+  handleTweetChange(event) {
+    this.setState({tweet: event.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={event => this.handleTweetChange(event)}  />
+        <p>{this.props.maxChars - this.state.tweet.length}</p>
       </div>
     );
   }
