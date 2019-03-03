8b82015d707365a925ab4a135dbc75b101c68998
lauradarlak <lauradarlak@gmail.com>
2019-03-02 19:43:38 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,7 +18,8 @@ class LoginForm extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    // login(this.state)
+    if (!this.state.username || !this.state.password) return
+    this.props.onSubmit(this.state)
   }
 
   render() {
