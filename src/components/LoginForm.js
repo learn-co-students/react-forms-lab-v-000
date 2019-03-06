1bb23d546d17f31ebd547212b4afdb03e9def288
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-06 20:12:19 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,8 +3,11 @@ import React from "react";
 class LoginForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      username: "",
+      password: ""
+    };
 
-    this.state = {};
   }
 
   render() {
@@ -13,13 +16,13 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInput}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInput}/>
           </label>
         </div>
         <div>
