fb6a5f576752a37741168f89517889496f8de84e
github username <github email address>
2019-03-28 20:12:15 -0700

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textField: ""
+    };
   }
+
+  handleChange = event => {
+    this.setState({
+        textField: event.target.value
+    })
+}
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" name="textField" onChange={this.handleChange} value={this.state.textField} />
+          <p>{this.props.maxChars - this.state.textField.length}</p>
       </div>
     );
   }
