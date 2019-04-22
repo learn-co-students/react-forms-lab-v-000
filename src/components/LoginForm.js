dac3401676dcc996922ef19b7885bc6071a7f186
Rollin <rollin.metzger@gmail.com>
2019-04-22 17:25:41 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,57 @@
 import React from "react";
 
-class LoginForm extends React.Component {
+export default class LoginForm extends React.Component {
   constructor() {
     super();
+    // basic login initial State
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
 
-    this.state = {};
+  // we need to handle the State change with our obj input(s)
+  handleChange = e => {
+    // as before, we can use a dynamic attr updater:
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  // we also require a function to handle form submission:
+  handleSubmit = e => {
+    e.preventDefault();
+    // here, we'll utilize a conditional to check for validation
+    if (!this.state.username || !this.state.password) return;
+    // handleLogin accesses the component using our submitted data, ie., State change
+    this.props.handleLogin(this.state);
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+              id="username"
+              name="username"
+              type="text"
+              onChange={e => this.handleChange(e)}
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={e => this.handleChange(e)}
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
@@ -30,4 +62,3 @@ class LoginForm extends React.Component {
   }
 }
 
-export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,37 @@
 import React from "react";
 
-class TwitterMessage extends React.Component {
+export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  //learning from the last lesson, we can use a dynamic State updating function
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={e => this.handleChange(e)}
+          value={this.state.message}
+        />
+        <p>Characters Remaining:
+          {this.props.maxChars - this.state.message.length}
+        </p>
       </div>
     );
   }
 }
 
-export default TwitterMessage;
