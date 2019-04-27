52af92e67ebdbb3fe274240ca0b8e3415b23cdff
carolgreene <carolgreene3@gmail.com>
2019-04-26 17:22:45 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,40 +4,54 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
+    //set initial value of state to empty strings
     this.state = {
-      username:'',
-      password:''
+      username: '',
+      password: ''
     };
   }
 
+  //update state w/every change of input
   handleChange = event => {
     this.setState({
-    [event.target.name]: event.target.value
+      [event.target.name]: event.target.value     //this will cover both username & password 
     })
   }
 
+  //when submit is hit, prevent default & call Login if both username & password are present
   handleSubmit = event => {
-    event.preventDefault();
+    event.preventDefault() 
+    if(this.state.username  && this.state.password )
+      this.props.handleLogin(this.state)            //handleLogin() is function in the parent, app
     
-    if (this.state.username && this.state.password) 
-      this.props.handleLogin(this.state)
   }
-
   
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={ event => this.handleSubmit(event) }>  {/*event listener when form is submitted*/}
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" onChange={this.handleChange}/>
+            <input 
+            id="username" 
+            name="username" 
+            type="text" 
+            value={this.state.username}    //tie form value to state to make controlled
+            onChange={this.handleChange}   //event listener for any input to username
+            />
           </label>
         </div>
         <div>
           <label>
-            Password
-            <input id="password" name="password" type="password" onChange={this.handleChange}/>
+          Password
+            <input 
+            id="password" 
+            name="password" 
+            type="password" 
+            value={this.state.password}    //tie form value to state to make controlled
+            onChange={this.handleChange}   //event listener for any input to password
+            />             
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,16 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
+    //set initial state to empty string
     this.state = {
       message: ''
     };
   }
 
+  //update state with every change of input
   handleChange = event => {
     this.setState({
-      message: event.target.value
+    message: event.target.value
     })
   }
 
@@ -19,8 +21,15 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleChange} />
-        <p>Characters left:{this.props.maxChars - this.state.message.length}</p>
+        <input 
+        type="text" 
+        name="message" 
+        id="message" 
+        value={this.state.message} //tie value of form to state to make controlled
+        onChange={event => this.handleChange(event)}   //add event listener & call handleChange()
+        />
+        {/*show remaining characters in the component using maxChars props from app minus the message length*/}
+        <p>Characters Left: {this.props.maxChars - this.state.message.length}</p> 
       </div>
     );
   }
