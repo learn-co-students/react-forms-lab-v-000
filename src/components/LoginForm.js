7b30a9ca3c2c33ff8ab9bb5fcbde3d6557a3586e
Naomi-Dennis <njdennis94@gmail.com>
2019-08-08 14:28:07 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,46 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+    	username: "",
+	password: "" 
+    };
   }
 
+
+  updateUsername(e){
+	this.setState({
+		username: e.target.value
+	})
+  }
+
+  updatePassword(e){
+	this.setState({
+		password: e.target.value
+	})
+  }
+  submitHandle(e){
+  	e.preventDefault();
+	if(this.state.username.length > 0 && this.state.password.length > 0){
+		this.props.handleLogin(); 
+	}
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitHandle.bind(this)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" value={this.state.username} name="username" type="text" onChange={this.updateUsername.bind(this)}  />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+	    <input id="password" value={this.state.password} name="password" type="password" onChange={this.updatePassword.bind(this)} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,18 +1,48 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+	maxChars: this.props.maxChars,
+	remainingChars: this.props.maxChars,
+	value: ""
+    };
   }
 
+  updateMaxChars(e){
+  
+  	/* Get the value of the input element */
+	let message = e.target.value 
+	
+	/* 
+	 * Find the reminaing characters left by subtracting the 
+	 * message's length by the number of maximum characters. 
+	 * 
+	 * */
+
+	let currentChars = message.length 
+	let remaining = this.state.maxChars - currentChars
+	
+	/*
+	 * 
+	 * Update the component's state.
+	 *
+	 * */
+	this.setState({
+		remainingChars: remaining, 
+		value: message
+	});
+  }
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+	      <strong>Your message ({this.state.remainingChars} characters remaining) :</strong>
+	      <input type="text" name="message" value={this.state.value} id="message"  onChange={this.updateMaxChars.bind(this)} />
+
+{console.log(this.state.value) }
+</div>
     );
   }
 }
