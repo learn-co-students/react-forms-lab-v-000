613659809cabd7be46416c190fc8f26f0179286b
Rafael Lopez <lpz.raf@gmail.com>
2019-08-25 18:04:47 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "   Test default input"
+    };
   }
+
+
+  handleInputChange = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <form onSubmit={event => this.handleSubmit(event)}>
+        <input type="text" onChange={event => this.handleInputChange(event)} name="message" id="message" value={this.state.message}
+ />      
+        </form>
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
