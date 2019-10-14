159321d259ce108bf1d44dce46e9196c749886e0
Conrad Bituin <38743840+cbituin@users.noreply.github.com>
2019-10-14 13:44:05 -0500

    Updates TwitterMessage.js

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,36 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      count: 0,
+      value: ""
+    };
+
+    this.changeHandler = this.changeHandler.bind(this);
   }
+
+  updateCounter = () => {
+    return this.props.maxChars - this.state.value.length;
+  };
+
+  changeHandler = event => {
+    this.setState = {
+      value: event.target.value
+    };
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <p>{this.updateCounter()} characters left.</p>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.value}
+          onChange={event => this.changeHandler(event)}
+        />
       </div>
     );
   }
