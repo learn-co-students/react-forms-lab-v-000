c216320b5ac506f8f7839a036d72436bd38e0eda
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-23 09:36:04 -0700

    Done.

---
+++ LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: ' ',
+      password: ' '
+    };
+  }
+
+  
