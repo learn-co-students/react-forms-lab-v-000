4314262dbfb38951c09bfe69abec251c4f34c77f
jamieberrier <berrier.jamie@gmail.com>
2020-08-25 13:42:49 -0700

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
+    let charsLeft = this.props.maxChars - this.state.message.length
+    
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+        onChange={this.handleChange}
+        value={this.state.message} />
+        <br></br>
+        {charsLeft} characters left
       </div>
     );
   }
